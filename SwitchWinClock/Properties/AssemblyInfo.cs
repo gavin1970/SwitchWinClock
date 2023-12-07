ceb3727d8f4ac6fff3f80f299eb4fa77bf3ede07
Switch <31280229+gavin1970@users.noreply.github.com>
2023-12-06 20:52:49 -0500

    Version update

--- SwitchWinClock/Properties/AssemblyInfo.cs
+++ SwitchWinClock/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("23.12.0.0")]
-[assembly: AssemblyFileVersion("23.12.6.211")]
-[assembly: AssemblyInformationalVersion("23.12.6.0")]
+[assembly: AssemblyFileVersion("23.12.7.152")]
+[assembly: AssemblyInformationalVersion("23.12.7.0")]
 [assembly: AssemblyKeyFileAttribute("")]
 [assembly: AssemblyDelaySignAttribute(false)]
